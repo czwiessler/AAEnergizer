cfb12bd7cb720a9c660e096ebe705967604093ae
Christian <christian.zwiessler@gmail.com>
2025-01-27 16:59:46 +0100

    updated branch for merge

--- src/models/lstm_24h_v2.py
+++ src/models/lstm_24h_v2.py
@@ -31,6 +31,7 @@ class UtilizationPredictor(pl.LightningModule):
         self.save_hyperparameters()  # optional, für Logging
 
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.dropout = nn.Dropout(0.3)
         # hier: output_size * forecast_horizon = Zahl der Target-Features * 24
         self.fc = nn.Linear(hidden_size, output_size * forecast_horizon)
         self.criterion = nn.MSELoss()
